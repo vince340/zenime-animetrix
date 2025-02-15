31e17fafee973bbf8056ceed972a2865204667d9
itzzzme <sayandas.devs@gmail.com>
2025-02-15 21:52:42 +0530

    Update getStreamInfo.utils.js

--- src/utils/getStreamInfo.utils.js
+++ src/utils/getStreamInfo.utils.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default async function getStreamInfo(animeId,episodeId,serverName,type) {
-  const api_url = import.meta.env.VITE_API_URL_2;
+  const api_url = import.meta.env.VITE_API_URL;
   try {
     const response = await axios.get(`${api_url}/stream?id=${animeId}?ep=${episodeId}&server=${serverName}&type=${type}`);
     return response.data.results;
