0cabe84fa5aecb761ee883792b437cdcd59a8804
animelegendpro <167015173+animelegendpro@users.noreply.github.com>
2025-02-23 16:08:49 +0530

    Update Error.jsx

--- src/components/error/Error.jsx
+++ src/components/error/Error.jsx
@@ -9,8 +9,8 @@ function Error({ error }) {
                 <img src="https://s1.gifyu.com/images/SBlOe.png" alt="" className="w-[300px] h-[300px] max-[500px]:w-[200px] max-[500px]:h-[200px]" />
                 <h1 className="text-white text-[35px] leading-5 mt-7">{error === "404" ? "404 Error" : "Error"}</h1>
                 <p className="mt-5">Oops! We couldn&apos;t find this page.</p>
-                <button className="bg-[#ffbade] py-2 px-4 w-fit rounded-3xl text-black text-light flex items-center gap-x-2 mt-7">
-                    <FaChevronLeft className="text-[#ffbade] w-[20px] h-[20px] rounded-full p-1 bg-black" />
+                <button className="bg-[#FF6300] py-2 px-4 w-fit rounded-3xl text-black text-light flex items-center gap-x-2 mt-7">
+                    <FaChevronLeft className="text-[#FF6300] w-[20px] h-[20px] rounded-full p-1 bg-black" />
                     <p onClick={() => navigate('/')} className="text-[18px]">Back to homepage</p>
                 </button>
             </div>
