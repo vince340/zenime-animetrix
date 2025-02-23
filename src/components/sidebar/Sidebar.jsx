3b96a4f0b4b13a0fb6954dcd64776ae2f9268178
animelegendpro <167015173+animelegendpro@users.noreply.github.com>
2025-02-23 17:29:14 +0530

    Update Sidebar.jsx

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -70,7 +70,7 @@ const Sidebar = ({ isOpen, onClose }) => {
               >
                 <FontAwesomeIcon
                   icon={item.icon}
-                  className="text-[#ffbade] text-xl font-bold max-[575px]:text-[15px]"
+                  className="text-[#FF6300] text-xl font-bold max-[575px]:text-[15px]"
                 />
                 <p className="text-[15px] max-[575px]:text-[13px]">
                   {item.label}
@@ -87,7 +87,7 @@ const Sidebar = ({ isOpen, onClose }) => {
                       index === 0 ? "rounded-l-[3px]" : "rounded-r-[3px]"
                     } ${
                       language === lang
-                        ? "bg-[#ffbade] text-black"
+                        ? "bg-[#FF6300] text-black"
                         : "bg-gray-600 text-white"
                     } max-[575px]:text-[9px] max-[575px]:py-0`}
                   >
